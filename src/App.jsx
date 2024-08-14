71edcef535794d057444d3c57c5afeb4f57c132a
Cachumbin <simoncarreno2006@gmail.com>
2024-08-14 17:22:53 -0500

    build: Basic web structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,39 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <header>
+        <div>
+          <select name="generation" id="generation-select">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <select name="type" id="type-select">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <input type="number" />
+        </div>
+        <div>
+          <input type="text" />
+        </div>
+      </header>
+      <main>
+        <div>
+          <ul>
+            <li>
+              <img src="" alt="" />
+              <h2></h2>
+              <p></p>
+              <p></p>
+              <p></p>
+            </li>
+          </ul>
+        </div>
+      </main>
+    </div>
   )
 }
 
