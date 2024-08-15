3a6118da16b8d0b6afcee71c5656da810c3d4e5a
Cachumbin <simoncarreno2006@gmail.com>
2024-08-15 08:48:08 -0500

    build(App.jsx): Created the structure of the filter

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,94 @@ const App = () => {
     <div>
       <header>
         <div>
+          <form action="">
+            <div>
+              <label htmlFor="search-bar">Search</label>
+              <input type="text" id="search-bar" />
+            </div>
+            <button>icono</button>
+          </form>
+        </div>
+      </header>
+      <main>
+      <div>
+          <p>Generation</p>
           <select name="generation" id="generation-select">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
           </select>
+          <p>Type</p>
           <select name="type" id="type-select">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
+            <option value="fire">Fire</option>
+            <option value="ice">Ice</option>
+            <option value="normal">Normal</option>
           </select>
-          <input type="number" />
+          <h3>Hp</h3>
+          <input type="number" /><input type="number" />
+          <h3>Attack</h3>
+          <input type="number" /><input type="number" />
+          <h3>Defense</h3>
+          <input type="number" /><input type="number" />
+          <button>Clear</button>
         </div>
-        <div>
-          <input type="text" />
-        </div>
-      </header>
-      <main>
         <div>
           <ul>
             <li>
-              <img src="" alt="" />
-              <h2></h2>
-              <p></p>
-              <p></p>
-              <p></p>
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt="" />
+              <h2>Ditto</h2>
+              <p>Type: Normal</p>
+              <p>Attack: 48</p>
+              <p>Hp: 48</p>
+              <p>Defense: 48</p>
+            </li>
+            <li>
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt="" />
+              <h2>Ditto</h2>
+              <p>Type: Normal</p>
+              <p>Attack: 48</p>
+              <p>Hp: 48</p>
+              <p>Defense: 48</p>
+            </li>
+            <li>
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt="" />
+              <h2>Ditto</h2>
+              <p>Type: Normal</p>
+              <p>Attack: 48</p>
+              <p>Hp: 48</p>
+              <p>Defense: 48</p>
+            </li>
+            <li>
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt="" />
+              <h2>Ditto</h2>
+              <p>Type: Normal</p>
+              <p>Attack: 48</p>
+              <p>Hp: 48</p>
+              <p>Defense: 48</p>
+            </li>
+            <li>
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt="" />
+              <h2>Ditto</h2>
+              <p>Type: Normal</p>
+              <p>Attack: 48</p>
+              <p>Hp: 48</p>
+              <p>Defense: 48</p>
+            </li>
+            <li>
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt="" />
+              <h2>Ditto</h2>
+              <p>Type: Normal</p>
+              <p>Attack: 48</p>
+              <p>Hp: 48</p>
+              <p>Defense: 48</p>
+            </li>
+            <li>
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt="" />
+              <h2>Ditto</h2>
+              <p>Type: Normal</p>
+              <p>Attack: 48</p>
+              <p>Hp: 48</p>
+              <p>Defense: 48</p>
             </li>
           </ul>
         </div>
